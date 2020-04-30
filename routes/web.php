d48c4d27f803838dcb9aa120f120c032e2d5373a
shamsuddin2021 <shamsuddin2021@gmail.com>
2020-05-01 00:28:29 +0600

    Display answers for question

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// 3/6 Done
+// 3/10 Done
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,5 +21,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
-
+// Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
