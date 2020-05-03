0778803b3bfef11aaad3159a6edc6ba8f7bb664f
shamsuddin2021 <shamsuddin2021@gmail.com>
2020-05-03 15:55:50 +0600

    Implement accept the answer as best answer

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// 3/14 Done
+// 4/1 Done
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,3 +24,4 @@ Route::resource('questions', 'QuestionsController')->except('show');
 // Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
